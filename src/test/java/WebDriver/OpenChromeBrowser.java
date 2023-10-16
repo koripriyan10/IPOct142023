0845adc2c9affbfd2500395148f964adb2e78329
Koripriyan <koripriyan@gmail.com>
2023-10-16 12:02:21 +0530

    commit Selenium chrome driver code launch

---
+++ src/test/java/WebDriver/OpenChromeBrowser.java
@@ -0,0 +1,26 @@
+package WebDriver;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class OpenChromeBrowser {
+	
+	WebDriver driver; 
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       
+		OpenChromeBrowser obj = new OpenChromeBrowser(); 
+		WebDriverManager.chromedriver().setup();
+		
+		obj.driver = new ChromeDriver();
+		obj.driver.get("https://www.flipkart.com/");
+		obj.driver.manage().window().maximize();
+		//obj.driver.manage().timeouts().pageLoadTimeout(60,TimeUnit.SECONDS);
+	}
+
+}
